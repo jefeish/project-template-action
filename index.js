431dcc5d0fff6f0b2cef50eb485094c77f8010d3
Jürgen Efeish <jefeish@github.com>
2021-11-04 04:28:20 +0000

    init

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const { Octokit } = require("@octokit/action");
 
 async function run() {
-    const octokit = new Octokit();
-    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    // const octokit = new Octokit();
+    // const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-    // See https://developer.github.com/v3/issues/#create-an-issue
-    const { data } = octokit.issues.create({
-        owner,
-        repo,
-        title: "My test issue",
-    });
+    // // See https://developer.github.com/v3/issues/#create-an-issue
+    // const { data } = octokit.issues.create({
+    //     owner,
+    //     repo,
+    //     title: "My test issue",
+    // });
     console.log("Issue created: %s", data);
 }
 
